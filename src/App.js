41262e4f625f451e8632d4a401882b2b6857d13e
Zulfugar <zulfugar@Zulfugars-iMac.local>
2024-02-17 07:41:43 +0200

    Pages imported and routes configured

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import Home from "./components/Home";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
+import Missing from "./components/Missing";
+import Admin from "./components/Admin";
+import Editor from "./components/Editor";
+import Lounge from "./components/Lounge";
 
 function App() {
   return (
     <HelmetProvider>
-      <div>
-        <Helmet>
-          <title>Auth</title>
-        </Helmet>
-        {/* Render Login or Register form down below */}
-        <Register />
-      </div>
+      <Helmet>
+        <title>Auth</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="linkpage" element={<LinkPage />} />
+            <Route path="unauthorized" element={<Unauthorized />} />
+            <Route path="*" element={<Missing />} />
+
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/lounge" element={<Lounge />} />
+
+        </Route>
+      </Routes>
     </HelmetProvider>
   );
 }
