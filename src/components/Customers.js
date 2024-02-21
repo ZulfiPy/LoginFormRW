db53984eef045a5fa6e9e3924f8ef368a6b35a2b
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-02-21 11:14:24 +0200

    table conditonal rendering + get data from backend /api/customers endpoint

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,77 +1,81 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react"
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const Customers = () => {
+    const [customerData, setCustomerData] = useState();
+    const axiosPrivate = useAxiosPrivate();
+
     const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log('getCustomers mounts');
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getCustomers = async () => {
+            try {
+                const response = await axiosPrivate.get('/api/customers', {
+                    signal: controller.signal
+                });
+                console.log('getCustomers', JSON.stringify(response.data));
+                isMounted && setCustomerData((response.data));
+            } catch (err) {
+                if (isMounted) {
+                    console.log('error in getCustomers', err);
+                    navigate('/login', { state: { from: location }, replace: true });
+                }
+            }
+        }
+
+        getCustomers()
+
+        return () => {
+            console.log('getCustomers unmounts');
+            isMounted = false;
+            controller.abort();
+        }
+    }, []);
 
     return (
         <section className="customers">
             <h1 className="page__title">VIEW CUSTOMERS</h1>
-            <table className="customers__table">
-                <thead>
-                    <tr>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>ISIKUKOOD</th>
-                        <th>Driver License Number</th>
-                        <th>Address</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Vehicle</th>
-                        <th>RUD</th>
-                    </tr>
-                </thead>
+            {customerData ? (
+                <table className="customers__table">
+                    <thead>
+                        <tr>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>ISIKUKOOD</th>
+                            <th>Driver License Number</th>
+                            <th>Address</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Vehicle</th>
+                            <th>RUD</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    <tr>
-                        <td>Ivan</td>
-                        <td>Ivanov</td>
-                        <td>39512310110</td>
-                        <td>EV059059</td>
-                        <td>Pirita, Tallinn, 10059</td>
-                        <td>ivan.petrov059@perm.com</td>
-                        <td>+37253059059</td>
-                        <td>749MJY</td>
-                        <td>RUD</td>
-                    </tr>
-
-                    <tr>
-                        <td>Ivan</td>
-                        <td>Petrov</td>
-                        <td>39512310110</td>
-                        <td>EV059059</td>
-                        <td>Pirita, Tallinn, 10059</td>
-                        <td>ivan.petrov059@perm.com</td>
-                        <td>+37253059059</td>
-                        <td>749MJY</td>
-                        <td>RUD</td>
-                    </tr>
-
-                    <tr>
-                        <td>Ivan</td>
-                        <td>Baskov</td>
-                        <td>39512310110</td>
-                        <td>EV059059</td>
-                        <td>Pirita, Tallinn, 10059</td>
-                        <td>ivan.petrov059@perm.com</td>
-                        <td>+37253059059</td>
-                        <td>749MJY</td>
-                        <td>RUD</td>
-                    </tr>
-
-                    <tr>
-                        <td>Ivan</td>
-                        <td>Petrov</td>
-                        <td>39512310110</td>
-                        <td>EV059059</td>
-                        <td>Pirita, Tallinn, 10059</td>
-                        <td>ivan.petrov059@perm.com</td>
-                        <td>+37253059059</td>
-                        <td>749MJY</td>
-                        <td>RUD</td>
-                    </tr>
-
-                </tbody>
-            </table>
+                    <tbody>
+                        {customerData.map((record, index) => (
+                            <tr key={index}>
+                                <td>{record.firstname}</td>
+                                <td>{record.lastname}</td>
+                                <td>{record.isikukood}</td>
+                                <td>{record.driverLicenseNumber}</td>
+                                <td>{record.address}</td>
+                                <td>{record.email}</td>
+                                <td>{record.phone}</td>
+                                <td>{record.vehicle ? record.vehicle : 'None'}</td>
+                                <td>RUD icons</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <h1>no data to display</h1>
+            )}
 
             <br />
             <Link className="effect__link" to="/">
