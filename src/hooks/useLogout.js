69fdb2bcd9d8bdb0fbad72a93148a6dd7140dc4f
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-02-22 09:00:15 +0200

    navigate to the login page after logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,10 @@
 import axios from "../api/axios";
 import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
     const { setAuth } = useAuth();
+    const navigate = useNavigate();
 
     const logout = async () => {
         setAuth({});
@@ -10,6 +12,7 @@ const useLogout = () => {
             const response = await axios.get('/logout', {
                 withCredentials: true
             });
+            navigate('/login');
             console.log('logout successfully done', JSON.stringify(response));
         } catch (err) {
             console.log('error while logging out', err);
