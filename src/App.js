0068bfba6be4a514834abb3527a408f29314098c
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-02-23 14:28:20 +0200

    react query added to app and configured with react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Routes, Route } from "react-router-dom";
 import ROLES_LIST from "./config/roles_list";
@@ -14,6 +15,9 @@ import User from "./components/User";
 import Customers from "./components/Customers";
 import Lounge from "./components/Lounge";
 import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
+
+const queryClient = new QueryClient();
 
 const ROLES = {
   'User': ROLES_LIST.User,
@@ -23,45 +27,49 @@ const ROLES = {
 
 function App() {
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>RW-Rent</title>
-      </Helmet>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="linkpage" element={<LinkPage />} />
-          <Route path="unauthorized" element={<Unauthorized />} />
-          <Route path="*" element={<Missing />} />
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <Helmet>
+          <title>RW-Rent</title>
+        </Helmet>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="linkpage" element={<LinkPage />} />
+            <Route path="unauthorized" element={<Unauthorized />} />
+            <Route path="*" element={<Missing />} />
 
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />} >
-            <Route path="/" element={<Home />} />
+            <Route element={<PersistLogin />}>
+              <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
+                <Route path="/" element={<Home />} />
+              </Route>
+
+              <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />} >
+                <Route path="admin" element={<Admin />} />
+              </Route>
+
+              <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />} >
+                <Route path="editor" element={<Editor />} />
+              </Route>
+
+              <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
+                <Route path="user" element={<User />} />
+              </Route>
+
+              <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Editor, ROLES.Admin]} />} >
+                <Route path="lounge" element={<Lounge />} />
+              </Route>
+
+              <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
+                <Route path="customers" element={<Customers />} />
+              </Route>
+            </Route>
+
           </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
-            <Route path="customers" element={<Customers />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />} >
-            <Route path="admin" element={<Admin />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />} >
-            <Route path="editor" element={<Editor />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor, ROLES.User]} />}>
-            <Route path="user" element={<User />} />
-          </Route>
-
-          <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Editor, ROLES.Admin]} />} >
-            <Route path="lounge" element={<Lounge />} />
-          </Route>
-
-        </Route>
-      </Routes>
-    </HelmetProvider>
+        </Routes>
+      </HelmetProvider>
+    </QueryClientProvider >
   );
 }
 
